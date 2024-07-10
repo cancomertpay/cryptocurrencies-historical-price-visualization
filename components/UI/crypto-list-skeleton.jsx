fad78e5aab027a90fac975c7f5d73977ae005494
Can Cömertpay <cancomertpay@gmail.com>
2024-07-10 12:06:50 +0300

    Update layout styles

--- components/UI/crypto-list-skeleton.jsx
+++ components/UI/crypto-list-skeleton.jsx
@@ -1,14 +1,7 @@
 function CryptoListSkeleton() {
   return (
-    <>
-      <p className="text-lg text-white/80 font-bold my-10 text-center">
-        Dear team leader or member, since the API is hosted in the free version
-        of render.com, if it is not used for a long time, it takes 50 seconds
-        for the API distributor server to open again. After 50 seconds, you can
-        use it in the normal flow without waiting. So this long waiting time is
-        not related to my application :). Thank you for your understanding.
-      </p>
-      <ul className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-5">
+    <div className="lg:min-h-[480px] mt-10 mx-5 pb-32">
+      <ul className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         <li className="w-full h-[160px] bg-black/90 animate-pulse rounded-xl p-5">
           <div className="mb-10">
             <div className="w-[6rem] h-[1rem] rounded-xl bg-gray-700 animate-pulse mb-2" />
@@ -80,7 +73,7 @@ function CryptoListSkeleton() {
           </div>
         </li>
       </ul>
-    </>
+    </div>
   );
 }
 
--- components/pairs-dashboard/pairs/pair-list.jsx
+++ components/pairs-dashboard/pairs/pair-list.jsx
@@ -18,15 +18,17 @@ function PairList({ data }) {
   }, [data]);
 
   return (
-    <ul className="grid gap-8 mt-36 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-5">
-      {sortedData.map((item) => (
-        <PairListItem
-          key={item.symbol}
-          symbol={item.symbol}
-          dataList={item.historicalPrice}
-        />
-      ))}
-    </ul>
+    <div className="lg:min-h-[480px] mt-10 mx-5 pb-32">
+      <ul className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        {sortedData.map((item) => (
+          <PairListItem
+            key={item.symbol}
+            symbol={item.symbol}
+            dataList={item.historicalPrice}
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
 
