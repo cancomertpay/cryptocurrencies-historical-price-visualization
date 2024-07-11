ce937b375371aa780e5ba83cb4e97554310a7947
Can Cömertpay <cancomertpay@gmail.com>
2024-07-11 12:34:37 +0300

    Use server actions

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,7 +1,13 @@
 import LoggedData from "@/components/admin-dashboard/logged-data";
+import AdminDashboardSkeleton from "@/components/UI/admin-dashboard-skeleton";
+import { Suspense } from "react";
 
 function AdminDashboard() {
-  return <LoggedData />;
+  return (
+    <Suspense fallback={<AdminDashboardSkeleton />}>
+      <LoggedData />
+    </Suspense>
+  );
 }
 
 export default AdminDashboard;
--- components/admin-dashboard/logged-data.jsx
+++ components/admin-dashboard/logged-data.jsx
@@ -1,47 +1,14 @@
-"use client";
-
-import { useState, useEffect } from "react";
 import LoggedDataTable from "./logged-data-table/logged-data-table";
-import AdminDashboardSkeleton from "../UI/admin-dashboard-skeleton";
 import { fetchLoggedData } from "@/lib/actions";
 
-function LoggedData() {
-  const [loggedData, setLoggedData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setIsLoading(true);
-    const fetchData = async () => {
-      try {
-        const data = await fetchLoggedData();
-        if (!data) {
-          throw new Error("Error fetching logged data");
-        }
-        setLoggedData(data);
-      } catch (error) {
-        console.error(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (!loggedData) return;
-
+async function LoggedData() {
+  const loggedData = await fetchLoggedData();
   return (
     <div className="lg:my-10 lg:mx-32">
-      {isLoading ? (
-        <AdminDashboardSkeleton />
-      ) : (
-        <>
-          <h1 className="text-3xl text-end p-4 bg-black/80 text-white font-bold border-b border-primary rounded-t-xl">
-            Logged Data
-          </h1>
-          <LoggedDataTable loggedData={loggedData} />
-        </>
-      )}
+      <h1 className="text-3xl text-end p-4 bg-black/80 text-white font-bold border-b border-primary rounded-t-xl">
+        Logged Data
+      </h1>
+      <LoggedDataTable loggedData={loggedData} />
     </div>
   );
 }
