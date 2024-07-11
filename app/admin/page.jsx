25173365f031bfa66cde2d3e2c7e467a828b4422
Can Cömertpay <cancomertpay@gmail.com>
2024-07-11 12:39:29 +0300

    Use server actions

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,6 +1,10 @@
+// react
+import { Suspense } from "react";
+
+// components
 import LoggedData from "@/components/admin-dashboard/logged-data";
 import AdminDashboardSkeleton from "@/components/UI/admin-dashboard-skeleton";
-import { Suspense } from "react";
+
 
 function AdminDashboard() {
   return (
