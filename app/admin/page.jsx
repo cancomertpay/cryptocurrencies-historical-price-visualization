0c7520dfc1452fa0701f78448664b225226de681
Can Cömertpay <cancomertpay@gmail.com>
2024-07-11 12:56:57 +0300

    Fix revalidatePath bug

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,17 +1,7 @@
-// react
-import { Suspense } from "react";
-
-// components
 import LoggedData from "@/components/admin-dashboard/logged-data";
-import AdminDashboardSkeleton from "@/components/UI/admin-dashboard-skeleton";
-
 
 function AdminDashboard() {
-  return (
-    <Suspense fallback={<AdminDashboardSkeleton />}>
-      <LoggedData />
-    </Suspense>
-  );
+  return <LoggedData />;
 }
 
 export default AdminDashboard;
--- components/admin-dashboard/logged-data.jsx
+++ components/admin-dashboard/logged-data.jsx
@@ -1,14 +1,47 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import LoggedDataTable from "./logged-data-table/logged-data-table";
+import AdminDashboardSkeleton from "../UI/admin-dashboard-skeleton";
 import { fetchLoggedData } from "@/lib/actions";
 
-async function LoggedData() {
-  const loggedData = await fetchLoggedData();
+function LoggedData() {
+  const [loggedData, setLoggedData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const data = await fetchLoggedData();
+        if (!data) {
+          throw new Error("Error fetching logged data");
+        }
+        setLoggedData(data);
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!loggedData) return;
+
   return (
     <div className="lg:my-10 lg:mx-32">
-      <h1 className="text-3xl text-end p-4 bg-black/80 text-white font-bold border-b border-primary rounded-t-xl">
-        Logged Data
-      </h1>
-      <LoggedDataTable loggedData={loggedData} />
+      {isLoading ? (
+        <AdminDashboardSkeleton />
+      ) : (
+        <>
+          <h1 className="text-3xl text-end p-4 bg-black/80 text-white font-bold border-b border-primary rounded-t-xl">
+            Logged Data
+          </h1>
+          <LoggedDataTable loggedData={loggedData} />
+        </>
+      )}
     </div>
   );
 }
